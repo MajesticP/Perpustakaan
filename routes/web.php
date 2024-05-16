77ff67cc9ac6de5c21bf1c1c6d4ebef4dfa18b94
MajesticP <122201340+MajesticP@users.noreply.github.com>
2024-05-16 13:17:31 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+Route::get('/anggota', [\App\Http\Controllers\AnggotaController::class, 'index'])->name('anggota.index');
+Route::post('/anggota', [\App\Http\Controllers\AnggotaController::class, 'store'])->name('anggota.store');
+
+Route::get('/buku', [\App\Http\Controllers\BukuController::class, 'index'])->name('buku.index');
+Route::post('/buku', [\App\Http\Controllers\BukuController::class, 'store'])->name('buku.store');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
